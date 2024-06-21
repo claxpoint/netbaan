77ac978b4fdd371976af478fdcc0d4f4ac4bf966
Moein Nikchehre <108075466+claxpoint@users.noreply.github.com>
2024-06-21 10:14:48 -0700

    Update netbaan.sh

--- netbaan.sh
+++ netbaan.sh
@@ -17,7 +17,7 @@ if [ -n "$SECONDARY_DNS" ]; then
 fi
 
 # Print confirmation message in green (temporary)
-echo -e "\e[32mDNS settings changed to: $PRIMARY_DNS"
+echo -e "\e[32mDNS settings changed to: $PRIMARY_DNS\e[0m"
 
 if [ -n "$SECONDARY_DNS" ]; then
   echo -e "\e[32mand $SECONDARY_DNS\e[0m"
