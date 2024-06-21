58064ff1792cbbf919125c09884d00c59cd1e27d
Moein Nikchehre <108075466+claxpoint@users.noreply.github.com>
2024-06-21 10:20:19 -0700

    Update netbaan.sh

--- netbaan.sh
+++ netbaan.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Print welcome message and script info in green
+echo -e "\033[92mWelcome to NetBaan!\033[0m"
+echo -e "\033[92mNetBaan - v1.0 - By Claxpoint\033[0m"
+echo -e "\033[92mFor more info or support, visit my GitHub: https://github.com/claxpoint\033[0m"
+
 # Get user input for primary and secondary DNS (emphasize both)
 echo "** Enter the IP addresses for your primary and secondary DNS servers (optional for secondary):"
 read -p "Primary DNS server IP: " PRIMARY_DNS
@@ -17,8 +22,8 @@ if [ -n "$SECONDARY_DNS" ]; then
 fi
 
 # Print confirmation message in green (temporary)
-echo "\e[32mDNS settings changed to: $PRIMARY_DNS\e[0m"
+echo -e "\e[32mDNS settings changed to: $PRIMARY_DNS\e[0m"
 
 if [ -n "$SECONDARY_DNS" ]; then
-  echo "\e[32mand $SECONDARY_DNS\e[0m"
+  echo -e "\e[32mand $SECONDARY_DNS\e[0m"
 fi
